5ddbf9a32ca00d051b3d598c4e188bb56f977b3c
soumodeeptocode <soumodeeptocode@gmail.com>
2024-07-22 09:46:09 +0530

    Initial commit

---
+++ src/main/java/com/scaler/oauth/security/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.scaler.oauth.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2Login();
+        return http.build();
+    }
+}
